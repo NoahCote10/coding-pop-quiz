93b5f16b460a286b4ebf454ddb02d4270c4765f4
Noah Cote <noahcote10@gmail.com>
2022-10-23 12:24:41 -0400

    linked js script to index. created object to hold questions. created query selectors.

---
+++ script.js
@@ -0,0 +1,41 @@
+const h1Tags = document.querySelectorAll("h1");
+const pTags = document.querySelectorAll("p");
+
+const quizQuestions = {
+    questionOne: {
+        question: "A very useful tool used during development and debugging for printinh content to the debugger is:",
+        answerOne: "1. JavaScript",
+        answerTwo: "2. terminal/bash",
+        answerThree: "3. for loops",
+        answerFour: "4. console.log"
+    },
+    questionTwo: {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        answerOne: "1. quotes",
+        answerTwo: "2. curly brackets",
+        answerThree: "3. parentheses",
+        answerFour: "4. square brackets"
+    },
+    questionThree: {
+        question: "Arrays in JavaScript can be used to store ____.",
+        answerOne: "1. numbers and strings",
+        answerTwo: "2. other arrays",
+        answerThree: "3. booleans",
+        answerFour: "4. all of the above"
+    },
+    questionFour: {
+        question: "Commonly used data types DO NOT include:",
+        answerOne: "1. strings",
+        answerTwo: "2. booleans",
+        answerThree: "3. alerts",
+        answerFour: "4. numbers"
+    },
+    questionFive: {
+        question: "String Values must be enclosed within ____ when being assigend to variables.",
+        answerOne: "1. commas",
+        answerTwo: "2. curly brackets",
+        answerThree: "3. quotes",
+        answerFour: "4. parentheses"
+    },
+}
+
